be2ee72ae6bd518de2f1b175da01feeb2a87a005
Ponomaryov Artyom <ponomaryovdev@gmail.com>
2026-05-29 00:59:32 +0300

    Add initial project structure with .gitignore, Dockerfile, app.py, and requirements.txt

---
+++ led/app.py
@@ -0,0 +1,26 @@
+import board
+import neopixel
+import time
+
+PIXEL_PIN = board.D18
+NUM_PIXELS = 8
+
+pixels = neopixel.NeoPixel(
+    PIXEL_PIN,
+    NUM_PIXELS,
+    brightness=1.0,
+    auto_write=False
+)
+
+while True:
+    pixels.fill((255, 0, 0))
+    pixels.show()
+    time.sleep(1)
+
+    pixels.fill((0, 255, 0))
+    pixels.show()
+    time.sleep(1)
+
+    pixels.fill((0, 0, 255))
+    pixels.show()
+    time.sleep(1)
